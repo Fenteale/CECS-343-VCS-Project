e7badc4149db6b93b91507c546d900941b0f4632
tigasbuto <55861530+tigasbuto@users.noreply.github.com>
2020-09-20 16:41:07 -0700

    finished i think?

---
+++ artID.js
@@ -0,0 +1,42 @@
+var fs = require('fs');
+var path = require('path');
+
+function createArtID(file) {
+	var artID;
+	var fileData = [];
+	var relativePath = path.dirname(file);
+	var readable = fs.createReadStream(file, {
+		encoding: 'utf8',
+		fd: null,
+	});
+	readable.on('readable', function() {
+		var artChar;
+		while(null !== (artChar = readable.read(1))) {
+			fileData.push(artChart);
+		}
+	});
+
+	var a = checkSum(fileData);
+	var b = lastFour(file.size);
+	var c = checkSum(relativePath);
+	artID = 'P' + a + '-' + 'L' + b + '-' + 'C' + c + '.txt'; 
+	return artID;
+}
+
+function checkSum() {
+	var weight;
+	for (var i = 0; i < arguments.length; i++++++++) {
+		weight += charCodeAt[arguments[i]]*1 + charCodeAt[arguments[i + 1]]*3 + charCodeAt[arguments[i + 2]]*7 + charCodeAt[arguments[i + 3]]*11;
+	};
+	lastFour(weight)
+	return lastweight;
+}
+
+function lastFour(x) {
+	if (x >= 1000) {
+		x = x.toString().slice(-4);
+	}
+	return x;
+}
+
+module.exports = { createArtID };
